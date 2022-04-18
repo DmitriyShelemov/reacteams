d57df42f9a489a69b173a189250d0a1bba5787eb
Dmitriy Shelemov <shelemov@yandex.ru>
2022-04-18 19:11:46 +0300

    Messages editor

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,15 +1,33 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import { useGetMessagesQuery } from "../../store/messages/message.api";
 import { IMessage } from "../../store/messages/message.types";
-import Message from './Message';
 
+import Message from './Message';
 import styles from './Messages.module.scss'
+
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from "slate-react"
+import { BaseEditor, Descendant } from 'slate'
+import { ReactEditor } from 'slate-react'
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue:any = []
 
 const Messages: FC = () => {
     const {activeChat} = useTypedSelector(state => state)
     const {data, isLoading, error } = useGetMessagesQuery(activeChat.chat?.id)
-
+    const [editor] = useState(() => withReact(createEditor()))
 
     return (
         <div className={styles.messages}>
@@ -28,8 +46,12 @@ const Messages: FC = () => {
                     </ul>
                 </div>
             )}
-
             
+            <div className={styles.editor}>
+                <Slate editor={editor} value={initialValue}>
+                    <Editable />
+                </Slate>
+            </div>
         </div>
     )
   }
